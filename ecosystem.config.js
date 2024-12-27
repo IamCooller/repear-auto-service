cdabfd191c3153426dca0b9c600d4f539b136ee0
IamCooller <45273146+IamCooller@users.noreply.github.com>
2024-12-27 14:02:34 -0700

    ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  apps: [
+    {
+      name: 'repear-auto-service',
+      script: 'pnpm',
+      args: 'run start',
+      env: {
+        PORT: 3050,
+        NODE_ENV: 'production'
+      },
+      instances: 1,
+      exec_mode: 'fork',
+      autorestart: true,
+      max_memory_restart: '400M',
+      max_restarts: 5
+    }
+  ]
+};
