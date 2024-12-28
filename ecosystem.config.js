5958d1912bb14b43700aa5f01d66386e95363ba0
IamCooller <45273146+IamCooller@users.noreply.github.com>
2024-12-28 12:25:42 -0700

    Update ecosystem.config.js to change port to 3055 and adjust memory limits

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,13 +8,14 @@ module.exports = {
       args: 'run start',
       watch: true,
       env: {
-        PORT: 3050,
+        PORT: 3055,
         NODE_ENV: 'production'
       },
       instances: 1,
       exec_mode: 'fork',
       autorestart: true,
-      max_memory_restart: '400M',
+      interpreter_args: '--max-old-space-size=1024', // Увеличим лимит до 1 ГБ
+      max_memory_restart: '700M', // Рестарт, если приложение превышает 700 MB
       max_restarts: 5
     }
   ]
